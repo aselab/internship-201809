354dc916cfbcbd2cda7137ac62d595a506b001b2
Toshiaki Yanagisawa <ty00369@ASE.CO.JP>
2018-09-21 17:46:47 +0900

    追加処理実装

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 import StickyNote from './StickyNote';
+import uuidv4 from 'uuid/v4'
+import { Button } from 'react-native-elements';
 
 export default class App extends React.Component {
+  state = {
+    StickyNoteList: [],
+  };
+   addStickyNote = () => {
+    const stickyNoteList = this.state.StickyNoteList;
+    const uuid = uuidv4();
+    stickyNoteList.push(
+      <StickyNote key={uuid} />
+    );
+    this.setState({ StickyNoteList: stickyNoteList });
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <StickyNote />
+      <View>
+        <View style={styles.container}>
+          {this.state.StickyNoteList}
+        </View>
+        <Button
+          title="＋"
+          onPress={this.addStickyNote}
+          buttonStyle={styles.addButton}
+        />
       </View>
     );
   }
@@ -14,6 +35,15 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    margin: 24,
-  }
+    marginTop: 10,
+    height: '100%'
+  },
+  addButton: {
+    width: 50,
+    height: 50,
+    left: 20,
+    bottom: 20,
+    position: 'absolute',
+    borderRadius: 50,
+  },
 });
